b2358f422df876d07d0acbbe1274b72e457009cc
Tiago Reis <tiago.reis@digestaid.health>
2024-12-17 13:49:31 +0000

    feat: Support data argument for GCS bucket seeding
    Signed-off-by: Tiago Reis <tiago.reis@digestaid.health>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 """Tests main conftest file."""
 
 import warnings
+from pathlib import Path
+from typing import Generator
+
+import pytest
 
 # import pytest_gcs.factories.noproc
 import pytest_gcs.factories.client
@@ -20,3 +24,20 @@ gcslocal1 = pytest_gcs.factories.client.gcslocal("gcs_proc1")
 
 gcs_proc2 = pytest_gcs.factories.proc.gcs_proc()
 gcslocal2 = pytest_gcs.factories.client.gcslocal("gcs_proc2")
+
+
+@pytest.fixture(scope="session")
+def seeded_data(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> Generator[Path, None, None]:
+    """Create seeded data."""
+    tmp_path = tmp_path_factory.mktemp("pytest-gcs-seeded")
+    bucket_dir = tmp_path / "bucket"
+    bucket_dir.mkdir()
+    bucket_file = bucket_dir / "test"
+    bucket_file.touch()
+    yield tmp_path
+
+
+gcs_proc_seeded = pytest_gcs.factories.proc.gcs_proc(data_fixture_name="seeded_data")
+gcslocal_seeded = pytest_gcs.factories.client.gcslocal("gcs_proc_seeded")
